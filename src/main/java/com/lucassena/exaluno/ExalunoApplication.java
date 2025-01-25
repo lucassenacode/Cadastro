bb84ec15fba5edd39ece472d9b324037621aea89
Lucas Sena <lucassenacode@gmail.com>
2025-01-25 20:09:51 -0300

    Project Created

---
+++ src/main/java/com/lucassena/exaluno/ExalunoApplication.java
@@ -0,0 +1,13 @@
+package com.lucassena.exaluno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExalunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExalunoApplication.class, args);
+	}
+
+}
